93e1594ab725293e1cf7e576cb386dfcfd2e58c5
kosowskie <56387911+kosowskie@users.noreply.github.com>
2021-01-23 18:51:56 +0100

    Lab10

---
+++ lab10/Lab10_multiprocessing.py
@@ -0,0 +1,13 @@
+import requests
+
+session = None
+
+def download_site(url, session):
+    with session.get(url) as response:
+        print(f"Read {len(response.content)} from {url}")
+
+
+def download_all_sites(sites):
+    with requests.Session() as session:
+        for url in sites:
+            download_site(url, session)
